c82c3df18e2ee86a985016489acac019b24affee
lauramedlock <laura.medlock@mail.utoronto.ca>
2021-05-30 09:06:07 -0400

    switched to batches for spacerL and connWeight

--- batch.py
+++ batch.py
@@ -7,11 +7,11 @@ params = specs.ODict()
 
 # fill in with parameters to explore and range of values (key has to coincide with a variable in simConfig)
 params['spacerL'] = [5, 25, 30, 50, 100]
+params['connWeight'] = [0.005, 0.01, 0.025, 0.05]
 params['cAMP'] = [0.005, 0.01, 0.05, 0.08, 0.1]
-#params['connWeight'] = [0.005, 0.01, 0.025, 0.05]
 
 # create Batch object with parameters to modify, and specifying files to use
-b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams_AIS.py',)
+b = Batch(params=params, cfgFile='cfg.py', netParamsFile='netParams.py',)
 
 # Set output folder, grid method (all param combinations), and run configuration
 b.batchLabel = 'disinhibition'
--- cfg.py
+++ cfg.py
@@ -13,29 +13,28 @@ cfg = specs.SimConfig()	# object of class SimConfig to store simulation configur
 ###############################################################################
 # SIMULATION PARAMETERS
 ###############################################################################
-cfg.duration = 2000 # Duration of the simulation, in ms
-cfg.dt = ais_variables.DT # Internal integration timestep to use
+cfg.duration = 2000         # Duration of the simulation, in ms
+cfg.dt = ais_variables.DT   # Internal integration timestep to use
 cfg.hParams = {'celsius': ais_variables.CELSIUS, 'v_init': -75.0, 'clamp_resist': 0.001}
-cfg.filename = 'ais'       # Set file output name
+cfg.filename = 'ais'        # Set file output name
 cfg.saveJson = True
 cfg.printPopAvgRates = True
 
 # Set recording traces
 #cfg.recordCells = {'all'}
-cfg.recordTraces = {'V_AIS':{'sec':'AIS','loc':0.5,'var':'v'}}
+cfg.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}}
 cfg.recordStep = 0.1                       
 
 # Save and display data
 #cfg.simLabel = 'model'
 #cfg.saveFolder = 'data' # re-enable this after batches are done
-#cfg.savePickle = False
 #cfg.saveJson = False
 #cfg.saveMat = True
 #cfg.saveDataInclude = ['simConfig', 'netParams']
 
 #cfg.analysis['plotTraces'] = {'include': [0,25,30,35,38,40], 'timeRange':[0,cfg.duration],'ylim':[-100,50], 'figSize':(10, 8), 'saveFig': True} 
 cfg.analysis['plotTraces'] = {'include':[25], 'saveFig':True}
-#cfg.analysis['plotRaster'] = {'timeRange':[0,cfg.duration], 'labels':'overlay', 'saveFig': True}
+cfg.analysis['plotRaster'] = {'timeRange':[0,cfg.duration], 'labels':'overlay', 'saveFig': True}
 #cfg.analysis['plotSpikeHist'] = {'include':['PAN'],'timeRange':[0,cfg.duration], 'binSize': 100, 'graphType':'bar','saveFig': True}
 #cfg.analysis['plot2Dnet'] = {'showConns': True,'saveFig': True}
 #cfg.analysis['plotShape'] = {'includePre':['PAN'], 'includePost':[],'showSyns': False,'dist': 0.8,'showFig': True}
@@ -44,9 +43,9 @@ cfg.analysis['plotTraces'] = {'include':[25], 'saveFig':True}
 cfg.saveDataInclude = ['simData', 'simConfig', 'netParams', 'net']
 
 # Variable parameters (used in netParams)
-cfg.spacerL = 5
-cfg.connWeight = 0.01
-cfg.cAMP = 0.005
+cfg.spacerL = 5        # spacer length between soma and AIS (inhibitory spinal neuron)
+cfg.connWeight = 0.01  # I to E connectivity weight
+cfg.cAMP = 0.005       #IClamp amplitude in netParams_AIS
 
 
 
